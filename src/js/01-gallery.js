c53244e672e22588b2ae0cd980a017836810f7f0
Maciek <maciekjozwicki@gmail.com>
2023-04-18 23:15:24 +0200

    task 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryEl = document.querySelector(".gallery");
 
-console.log(galleryItems);
+galleryItems.forEach((item) => {
+    const galleryItemEl = `
+    <li>
+        <a class="gallery__item" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" title="${item.description}"/>
+        </a>
+    </li>`
+
+    galleryEl.insertAdjacentHTML("beforeend", galleryItemEl)
+console.log(galleryItemEl)
+})
+
+let galleryTEST = new SimpleLightbox('.gallery a', { captionDelay : 250 });
+galleryTEST.on('show.simplelightbox', function () {
+});
+
+
+
+
+
+
+console.log(galleryItems, galleryEl);
