ab6db0b9e3b323db650f60a0beff3294644874dc
Maciek <maciekjozwicki@wp.pl>
2023-04-25 21:30:07 +0200

    finish task 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,5 @@
 import Player from '@vimeo/player'
+import throttle from 'lodash.throttle'
 
 const VideoEl = document.getElementById('vimeo-player')
 const player = new Player(VideoEl)
@@ -8,8 +9,15 @@ const player = new Player(VideoEl)
 //         console.log(data.seconds);
 // });
 
-player.on('timeupdate', (data) => {
-    localStorage.setItem("videoplayer-current-time", JSON.stringify(data.seconds))
-})
-    
+// player.on('timeupdate', throttle(data => {
+//     localStorage.setItem("videoplayer-current-time", JSON.stringify(data.seconds)), 1000
+// }))
+  
+player.on('timeupdate', throttle(data => {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
+
+
+
 player.setCurrentTime(localStorage.getItem("videoplayer-current-time"))
