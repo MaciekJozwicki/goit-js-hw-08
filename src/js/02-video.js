7cac02e78914b642f38abbc9303d80340b42a385
Maciek <maciekjozwicki@wp.pl>
2023-04-25 20:59:15 +0200

    update 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,15 @@
-import Player from '@vimeo/player';
+import Player from '@vimeo/player'
+
 const VideoEl = document.getElementById('vimeo-player')
 const player = new Player(VideoEl)
 
 
-   
+// const dataTime = player.on('timeupdate', function(data) {
+//         console.log(data.seconds);
+// });
 
-
-
-
-
-
-
-console.log(player)
+player.on('timeupdate', (data) => {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data.seconds))
+})
+    
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"))
