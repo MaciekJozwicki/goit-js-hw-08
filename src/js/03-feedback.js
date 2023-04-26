c31c7411286db41dac2388ed4d4dc5c6824f3eee
Maciek <maciekjozwicki@wp.pl>
2023-04-26 18:46:40 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
 
+import throttle from "lodash.throttle"
+
+const formEl = document.querySelector('.feedback-form')
+const emailInputEl = document.querySelector('input[type="email"]')
+const messageInputEl = document.querySelector('textarea[name="message"]')
+const buttonEl = document.querySelector('button[type="submit"]')
+
+// emailInputEl.addEventListener("input", (event) => {
+//         console.log(event.currentTarget.value)
+// })
+
+
+// messageInputEl.addEventListener("input", (event) => {
+//         console.log(event.currentTarget.value)
+// })
+
+const data = {
+    email: '',
+    message: '',
+};
+
+const getData = (e) => {
+    data[e.target.name] = e.target.value
+    localStorage.setItem('feedback-form-state', JSON.stringify(data))
+}
+
+const submit = (e) => {
+    e.preventDefault()
+    localStorage.clear()
+    formEl.reset()
+    console.log(data)
+}
+
+
+formEl.addEventListener("input", throttle(getData, 500))
+buttonEl.addEventListener("click", submit)
+
+emailInputEl.textContent = localStorage.getItem('feedback-form-state.email')
