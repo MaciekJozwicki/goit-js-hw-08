1837f55dd1f7611273f13ff875e91598da98c23a
Maciek <maciekjozwicki@wp.pl>
2023-05-01 19:50:05 +0200

    update task 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,20 +2,37 @@
 import throttle from "lodash.throttle"
 
 const formEl = document.querySelector('.feedback-form')
-const emailInputEl = document.querySelector('input[type="email"]')
+const emailInputEl = document.querySelector('input[name="email"]')
 const messageInputEl = document.querySelector('textarea[name="message"]')
 const buttonEl = document.querySelector('button[type="submit"]')
-
 
 const data = {
     email: '',
     message: '',
 };
 
-const getData = (e) => {
-    data[e.target.name] = e.target.value
+const saveState = () => {
+    data.email = emailInputEl.value
+    data.message = messageInputEl.value
+
     localStorage.setItem('feedback-form-state', JSON.stringify(data))
 }
+
+const loadState = () => {
+    const loaded = JSON.parse(localStorage.getItem('feedback-form-state'))
+
+    if (loaded) {
+        emailInputEl.value = loaded.email
+        messageInputEl.value = loaded.message
+        // data.email = loaded.email
+        // data.message = loaded.message
+    } else {
+        emailInputEl.value = ""
+        messageInputEl.value = ""
+    }
+}
+
+loadState()
 
 const submit = (e) => {
     e.preventDefault()
@@ -25,6 +42,6 @@ const submit = (e) => {
 }
 
 
-formEl.addEventListener("input", throttle(getData, 500))
+formEl.addEventListener("input", throttle(saveState, 500))
 buttonEl.addEventListener("click", submit)
 
